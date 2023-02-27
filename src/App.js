569eae38ae661857d4e2579b67e095074bfd6e86
alexvallaro <alexvallaro@gmail.com>
2023-02-26 22:17:03 -0300

    ILC

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import second, { NavBar } from "./components/Navbar";
+import ItemListContainer from './components/ItemListContainer/indexILC';
 
 function App() {
   return (
+    <>
     <NavBar />
+    <ItemListContainer />
+    </>
   );
 }
 
---
+++ src/components/ItemListContainer/indexILC.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Title from "../Title/indexT";
+
+export const ItemListContainer = () => {
+    return (
+        <Title greeting="Brodi, Indumentaria Masculina" />
+    );
+}
+
+export default ItemListContainer;
